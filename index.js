c5f7d01999463964efe3ef71c983b96f4937d058
i5ting <i5ting@126.com>
2016-09-14 21:20:13 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ global.$dep = {
   
 }
 
+var path = require('path')
 var Inflector = require('inflected');
+var debug = require('debug')('exportall')
 
 module.exports = function (package_file) {
+  debug(package_file + '/package.json')
+  
   var dependencies = require(package_file + '/package.json')['dependencies']
   
   for (var module_name in dependencies) {
     _name = module_name.replace(/-/g, '_')
     _name = Inflector.classify(_name)
-    // console.log(_name)
+    
+    debug(_name)
     
     $dep[_name] = require(module_name)
   }
